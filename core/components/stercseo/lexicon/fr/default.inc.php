d9f96e96c2b96968839a2089726e96de0847559c
Romain <romain@melting-media.com>
2013-11-07 21:36:52 +0100

    French translation

---
+++ core/components/stercseo/lexicon/fr/default.inc.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * StercSEO French language file
+ *
+ * @package stercseo
+ * @subpackage lexicon
+ */
+
+$_lang['stercseo.seo'] = 'StercSEO';
+
+//Tab Findability
+
+$_lang['stercseo.findability'] = 'Trouvabilité';
+
+$_lang['stercseo.index'] = 'Inclure dans les moteurs de recherche';
+$_lang['stercseo.index_yes'] = 'Oui, cette page peut être indéxée';
+$_lang['stercseo.index_no'] = 'Non, cette page ne doit pas être indéxée (noindex)';
+$_lang['stercseo.index_desc'] = 'Note : certaines pages de votre site ne doivent pas être visibles dans les moteurs de recherche et plan de site (sitemap). Par exemple : disclaimer, termes & conditions d\'utilisation, politique de confidentialité.';
+
+$_lang['stercseo.follow'] = 'Suivre les liens';
+$_lang['stercseo.follow_yes'] = 'Oui, suivre les liens de cette page';
+$_lang['stercseo.follow_no'] = 'Non, ne pas suivre les liens de cette page (nofollow)';
+$_lang['stercseo.follow_desc'] = 'Définissez ci les moteurs de recherche doivent (ou ne doivent pas) suivre les liens de cette page';
+
+$_lang['stercseo.searchable'] = 'Inclure les pages dans la recherche interne du site';
+$_lang['stercseo.searchable_yes'] = 'Oui, inclure cette page dans la recherche interne';
+$_lang['stercseo.searchable_no'] = 'Non, ne pas inclure cette page dans la recherche interne';
+$_lang['stercseo.searchable_desc'] = 'Indiquez peut être inclue dans la recherche interne de votre site. Un exemple de page qui peut être superflue : la page de remerciement d\'un formulaire de contact.';
+
+//Tab Sitemap
+$_lang['stercseo.sitemap'] = 'Google Sitemap';
+
+$_lang['stercseo.sitemap_include'] = 'Inclure les pages dans Google Sitemap';
+$_lang['stercseo.sitemap_include_yes'] = 'Oui, inclure cette page dans Google Sitemap';
+$_lang['stercseo.sitemap_include_no'] = 'Non, masquer cette page dans Google Sitemap';
+$_lang['stercseo.sitemap_include_desc'] = 'Indiquez si vous souhaitez que cette page apparaîsse dans le plan du site (sitemap) pour Google';
+
+$_lang['stercseo.priority'] = 'Priorité';
+$_lang['stercseo.priority_important'] = '1.0 - Élevée';
+$_lang['stercseo.priority_normal'] = '0.5 - Normale';
+$_lang['stercseo.priority_nopriority'] = '0.25 - Faible';
+$_lang['stercseo.priority_desc'] = 'En indiquant un niveau de priorité, vous indiquz aux moteurs de recherche l\'importance de la page. Veuillez noter les moteurs de recherche n\'accepteront pas "aveuglément" vos priorités.';
+
+$_lang['stercseo.changefreq'] = 'Fréquence de mise à jour';
+$_lang['stercseo.changefreq_daily'] = 'Journalière';
+$_lang['stercseo.changefreq_weekly'] = 'Hebdomadaire';
+$_lang['stercseo.changefreq_monthly'] = 'Mensuelle';
+$_lang['stercseo.changefreq_desc'] = 'Indiquez la fréquence à laquelle vous estimez que le contenu de cette page sera modifié.';
+
+
+//Tab Redirects
+$_lang['stercseo.redirects'] = 'Redirections 301';
+$_lang['stercseo.uri_add'] = 'Ajouter l\'ancienne URL';
+$_lang['stercseo.uri_header'] = 'Indiquez ci-dessous la liste des anciennes URLs de cette page';
+$_lang['stercseo.grid_noresults'] = '<h4>Aucune redirection</h4><p>Il n\'y a aucune redirection pour cette page.</p>';
+$_lang['stercseo.redirects_desc'] = 'Un changement sur votre page affecte les moteurs de recherche. Changer l\'URL d\'une page entraînera la perte de la valeur acquise auprès des moteurs de recherche. Avec des redirections 301, vous ne perdez pas cette valeur. StercSEO ajoute automatiquement des redirections 301 lorsque l\'URL d\'une page change.';
+$_lang['stercseo.alreadyexists'] = '[[++site_URI]]<strong>[[+URI]]</strong> a déjà été ajouté à la page : <strong>[[+pagetitle]] ([[+id]])</strong>';
+
+//Tab Freeze URL
+$_lang['stercseo.freeze_uri'] = 'URL fixe';
+$_lang['stercseo.uri_override'] = 'Indiquez une URL fixe pour cette page';
+$_lang['stercseo.uri_after'] = 'URL après ';
+
+
+
+
+
+
+
+
