8221470a90fc0f2a99cdde94cd1acfb7e1c1030b
Sander <sander@Sanders-iMac.local>
2015-12-18 11:17:51 +0100

    Added option to set outerTpl and rowTpl to sitemap snippet

--- core/components/stercseo/elements/snippets/StercSeoSiteMap.snippet.php
+++ core/components/stercseo/elements/snippets/StercSeoSiteMap.snippet.php
@@ -1,6 +1,9 @@
 <?php
 require_once $modx->getOption('stercseo.core_path',null,$modx->getOption('core_path').'components/stercseo/').'model/stercseo/stercseo.class.php';
-$stercseo = new StercSeo($modx,$scriptProperties);
-$allowSymlinks = (isset($allowSymlinks)) ? $allowSymlinks : 0;
-$contexts = (isset($contexts)) ? explode(',',str_replace(' ','',$contexts)) : array($modx->resource->get('context_key'));
-return $stercseo->sitemap($contexts, 'sitemap/rowTpl', 'sitemap/outerTpl', $allowSymlinks);
+$stercseo       = new StercSeo($modx,$scriptProperties);
+$allowSymlinks  = (isset($allowSymlinks)) ? $allowSymlinks : 0;
+$contexts       = (isset($contexts)) ? explode(',',str_replace(' ','',$contexts)) : array($modx->resource->get('context_key'));
+$outerTpl       = (isset($outerTpl)) ? $outerTpl : 'sitemap/outerTpl';
+$rowTpl         = (isset($rowTpl)) ? $rowTpl : 'sitemap/rowTpl';
+
+return $stercseo->sitemap($contexts, $rowTpl, $outerTpl, $allowSymlinks);
