bd7dc9249d06d6330cef28e2dd042eece28359f8
Rinze van der Brug <rinze@sterc.nl>
2017-02-20 14:23:52 +0100

    Changed lexicons

--- core/components/stercseo/lexicon/en/default.inc.php
+++ core/components/stercseo/lexicon/en/default.inc.php
@@ -8,41 +8,40 @@
  * @subpackage lexicon
  */
 
-$_lang['stercseo.seo'] = 'StercSEO';
+$_lang['stercseo.seo'] = 'SEO Tab';
 $_lang['stercseo.seotab'] = 'SEO Tab';
 $_lang['stercseo.menu_desc'] = 'Manage all your SEO Tab 301 redirects.';
 
 //Tab Findability
 $_lang['stercseo.findability'] = 'Findability';
+$_lang['stercseo.index'] = 'Include this page in search engines? (index/noindex)';
+$_lang['stercseo.index_yes'] = 'Yes (index)';
+$_lang['stercseo.index_no'] = 'No (noindex)';
+$_lang['stercseo.index_desc'] = 'Irrelevant pages should not be indexed in search engines. Irrelevant page examples: disclaimer, terms and conditions, privacy policy.';
 
-$_lang['stercseo.index'] = 'Include in search engines (index/noindex)';
-$_lang['stercseo.index_yes'] = 'Yes, include in search engines (index)';
-$_lang['stercseo.index_no'] = 'No, do not include in search engines (noindex)';
-$_lang['stercseo.index_desc'] = 'Irrelevant pages shouldn\'t be indexed in search engines. Irrelevant page examples: disclaimer, terms and conditions, privacy policy.';
+$_lang['stercseo.follow'] = 'Follow links on this page? (follow/nofollow)';
+$_lang['stercseo.follow_yes'] = 'Yes (follow)';
+$_lang['stercseo.follow_no'] = 'No (nofollow)';
+$_lang['stercseo.follow_desc'] = 'Should search engines follow links on this page?';
 
-$_lang['stercseo.follow'] = 'Follow links (follow/nofollow)';
-$_lang['stercseo.follow_yes'] = 'Yes, follow links on this page (follow)';
-$_lang['stercseo.follow_no'] = 'No, do not follow links on this page (nofollow)';
-$_lang['stercseo.follow_desc'] = 'Should search engines follow links on this page.';
-
-$_lang['stercseo.searchable'] = 'Include in internal search results';
-$_lang['stercseo.searchable_yes'] = 'Yes, include in internal search results';
-$_lang['stercseo.searchable_no'] = 'No, exclude from internal search results';
-$_lang['stercseo.searchable_desc'] = 'Should this page be included in the internal search results of your website. "Thank you"-pages are good examples of pages which should be excluded.';
+$_lang['stercseo.searchable'] = 'Include this page in internal search results?';
+$_lang['stercseo.searchable_yes'] = 'Yes';
+$_lang['stercseo.searchable_no'] = 'No';
+$_lang['stercseo.searchable_desc'] = '"Thank you"-pages and "404 not found"-pages are good examples of pages which should NOT be included.';
 
 //Tab Sitemap
 $_lang['stercseo.sitemap'] = 'Google Sitemap';
 
-$_lang['stercseo.sitemap_include'] = 'Include pages in the Google Sitemap';
-$_lang['stercseo.sitemap_include_yes'] = 'Yes, include this page in the Google Sitemap';
-$_lang['stercseo.sitemap_include_no'] = 'No, hide this page for the Google Sitemap';
-$_lang['stercseo.sitemap_include_desc'] = 'Set whether this page may or may not be included in the sitemap for Google';
+$_lang['stercseo.sitemap_include'] = 'Include this page in the Google Sitemap?';
+$_lang['stercseo.sitemap_include_yes'] = 'Yes';
+$_lang['stercseo.sitemap_include_no'] = 'No';
+$_lang['stercseo.sitemap_include_desc'] = 'Set whether this page may or may not be included in the sitemap for Google.';
 
 $_lang['stercseo.priority'] = 'Priority';
 $_lang['stercseo.priority_important'] = '1.0 - High';
 $_lang['stercseo.priority_normal'] = '0.5 - Normal';
 $_lang['stercseo.priority_nopriority'] = '0.25 - Low';
-$_lang['stercseo.priority_desc'] = 'Set the priority level to indicate to search engines how important a page is. Higher is more important. Please note: search engines will not blindly accept your priority level.';
+$_lang['stercseo.priority_desc'] = 'Set the priority level to indicate to search engines how important this page is. Higher is more important. Please note: search engines will not blindly accept your priority level!';
 
 $_lang['stercseo.changefreq'] = 'Update frequency';
 $_lang['stercseo.changefreq_daily'] = 'Daily';
@@ -52,34 +51,34 @@ $_lang['stercseo.changefreq_desc'] = 'Specify how often you expect the content o
 
 //Tab Redirects
 $_lang['stercseo.redirects'] = '301 Redirects';
-$_lang['stercseo.uri_add'] = 'Create 301 redirect';
-$_lang['stercseo.uri_header'] = 'Old URLs redirecting to this page.';
+$_lang['stercseo.uri_add'] = 'Create a 301 redirect';
+$_lang['stercseo.uri_header'] = 'Old URLs redirecting to this page:';
 $_lang['stercseo.grid_noresults'] = '<h4>No redirects</h4><p>There are no redirects set for this page.</p>';
-$_lang['stercseo.redirects_desc'] = 'Any change on your page affects search engines, but changing the URL of a page without adding a 301 redirect, will result in losing ALL acquired value. With SEO Tab\'s 301 redirects your search engine value will be retained! To help prevent this, SEO Tab automatically adds 301 redirects when you change the URL of a page.';
+$_lang['stercseo.redirects_desc'] = 'Changing the URL of a page without adding a 301 redirect, will result in losing ALL acquired search engine value of that page. With SEO Tab\'s 301 redirects that value will be retained! To help you, SEO Tab automatically adds 301 redirects when you change the URL of a page. To delete a 301 redirect, right-click on it.';
 $_lang['stercseo.alreadyexists'] = '<b>[[+url]]</b> has already been added to resource: <b>[[+pagetitle]] (id: [[+id]]) - <a href="[[+link]]" target="_blank">Edit in new window</a></b>';
 $_lang['stercseo.uri_label'] = 'Old URL';
 $_lang['stercseo.uri_label_desc'] = 'Enter the full URL, including your domain. Example: "https://www.google.com/old-pages/about-us".';
-$_lang['stercseo.url_missing_protocol'] = 'Incorrect url. Please add http:// or https://';
+$_lang['stercseo.url_missing_protocol'] = 'Incorrect URL. Please add: http:// or https://';
 
 //Tab Freeze URL
 $_lang['stercseo.freeze_uri'] = 'Freeze URL';
-$_lang['stercseo.uri_override'] = 'Set a Freeze URL for this page';
+$_lang['stercseo.uri_override'] = 'Set a Freeze URL for this page.';
 $_lang['stercseo.uri_after'] = 'URL after ';
-$_lang['stercseo.uri_after_desc'] = 'Freeze URLs can be used to create short URLs. 
-For example, to set this page URL to [[+site_url]]short-url", enter "short-url" in the field below.';
+$_lang['stercseo.uri_after_desc'] = 'Freeze URLs can be used to create user-friendly URLs. 
+For example, to set this page URL to [[+site_url]]user-friendly-url", enter "user-friendly-url" in the field above.';
 
 //Settings
 $_lang['setting_stercseo.context-aware-alias'] = '301 Redirects are unique per context';
-$_lang['setting_stercseo.context-aware-alias_desc'] = 'Make old urls unique to context';
-$_lang['setting_stercseo.index'] = 'Default resource setting: Include in search engines';
+$_lang['setting_stercseo.context-aware-alias_desc'] = 'Make old URLs unique to context';
+$_lang['setting_stercseo.index'] = 'Default resource setting: include in search engines';
 $_lang['setting_stercseo.index_desc'] = 'Include new pages in search engines per default';
-$_lang['setting_stercseo.follow'] = 'Default resource setting: Following links';
+$_lang['setting_stercseo.follow'] = 'Default resource setting: following links';
 $_lang['setting_stercseo.follow_desc'] = 'Follow links on new pages per default';
-$_lang['setting_stercseo.sitemap'] = 'Default resource setting: Include pages in the Google Sitemap';
-$_lang['setting_stercseo.sitemap_desc'] = 'Include new pages in sitemap.xml per default';
-$_lang['setting_stercseo.priority'] = 'Default resource setting: Priority';
+$_lang['setting_stercseo.sitemap'] = 'Default resource setting: include pages in Google Sitemap';
+$_lang['setting_stercseo.sitemap_desc'] = 'Include new pages in Google Sitemap per default';
+$_lang['setting_stercseo.priority'] = 'Default resource setting: priority';
 $_lang['setting_stercseo.priority_desc'] = 'Priority of page in sitemap.xml (0.25 or 0.5 or 1)';
-$_lang['setting_stercseo.changefreq'] = 'Default resource setting: Update frequency';
+$_lang['setting_stercseo.changefreq'] = 'Default resource setting: update frequency';
 $_lang['setting_stercseo.changefreq_desc'] = 'Default frequency (daily, weekly, monthly)';
 $_lang['setting_stercseo.hide_from_usergroups'] = 'Hide SEO Tab from these usergroups';
 $_lang['setting_stercseo.hide_from_usergroups_desc'] = 'Comma separated list of usergroups who are not allowed to access SEO Tab';
@@ -87,8 +86,8 @@ $_lang['setting_stercseo.hide_from_usergroups_desc'] = 'Comma separated list of 
 // CMP
 $_lang['stercseo.redirects.description'] = 'Manage your SEO Tab 301 redirects. 
 Redirects can also be added when editing a resource by clicking the tab called "SEO".';
-$_lang['stercseo.redirects.window_title'] = 'Add redirect url';
-$_lang['stercseo.uri'] = 'Old url (url to redirect)';
+$_lang['stercseo.redirects.window_title'] = 'Add redirect URL';
+$_lang['stercseo.uri'] = 'Old URL (URL to redirect)';
 $_lang['stercseo.target'] = 'New URL / Resource';
 $_lang['stercseo.uri_update'] = 'Update redirect';
 $_lang['stercseo.uri_remove'] = 'Remove redirect';
--- core/components/stercseo/lexicon/nl/default.inc.php
+++ core/components/stercseo/lexicon/nl/default.inc.php
@@ -8,95 +8,94 @@
  * @subpackage lexicon
  */
 
-$_lang['stercseo.seo'] = 'StercSEO';
+$_lang['stercseo.seo'] = 'SEO Tab';
 $_lang['stercseo.seotab'] = 'SEO Tab';
 $_lang['stercseo.menu_desc'] = 'Beheer al je SEO Tab 301 redirects.';
 
 //Tab Findability
 $_lang['stercseo.findability'] = 'Vindbaarheid';
 
-$_lang['stercseo.index'] = 'Opnemen in zoekmachines (index/noindex)';
-$_lang['stercseo.index_yes'] = 'Ja, deze pagina mag geïndexeerd worden (index)';
-$_lang['stercseo.index_no'] = 'Nee, deze pagina mag niet geïndexeerd worden (noindex)';
-$_lang['stercseo.index_desc'] = 'Let op: irrelevante pagina\'s van je site moeten worden weggelaten uit de zoekmachines én sitemap. Voorbeelden van irrelevante pagina\'s zijn: disclaimer, algemene voorwaarden, privacy policy.';
+$_lang['stercseo.index'] = 'Pagina opnemen in zoekmachines? (index/noindex)';
+$_lang['stercseo.index_yes'] = 'Ja (index)';
+$_lang['stercseo.index_no'] = 'Nee (noindex)';
+$_lang['stercseo.index_desc'] = 'Let op: het is beter om irrelevante pagina\'s van je website niet op te nemen in zoekmachines. Voorbeelden van irrelevante pagina\'s zijn: disclaimer, algemene voorwaarden, privacy policy.';
 
-$_lang['stercseo.follow'] = 'Links volgen (follow/nofollow)';
-$_lang['stercseo.follow_yes'] = 'Ja, volg links op deze pagina (follow)';
-$_lang['stercseo.follow_no'] = 'Nee, geen links volgen op deze pagina (nofollow)';
-$_lang['stercseo.follow_desc'] = 'Stel in of zoekmachines de links op deze pagina wel of niet mogen volgen';
+$_lang['stercseo.follow'] = 'Links op deze pagina volgen? (follow/nofollow)';
+$_lang['stercseo.follow_yes'] = 'Ja (follow)';
+$_lang['stercseo.follow_no'] = 'Nee (nofollow)';
+$_lang['stercseo.follow_desc'] = 'Stel in of zoekmachines de links op deze pagina wel of niet mogen volgen.';
 
-$_lang['stercseo.searchable'] = 'Opnemen in interne zoekmachine';
-$_lang['stercseo.searchable_yes'] = 'Ja, deze pagina opnemen in de interne zoekmachine';
-$_lang['stercseo.searchable_no'] = 'Nee, verberg deze pagina voor de interne zoekmachine';
-$_lang['stercseo.searchable_desc'] = 'Stel in of deze pagina mag worden opgenomen in de interne zoekresultaten van je website. Een voorbeeld van een pagina die overbodig is in je zoekresultaten is de bedankpagina van een contactformulier';
+$_lang['stercseo.searchable'] = 'Pagina opnemen in interne zoekresultaten?';
+$_lang['stercseo.searchable_yes'] = 'Ja';
+$_lang['stercseo.searchable_no'] = 'Nee';
+$_lang['stercseo.searchable_desc'] = '"Bedankt"-pagina\'s en "404: niet gevonden"-pagina\'s zijn goede voorbeelden van pagina\'s die je beter niet kunt opnemen in je interne zoekresultaten.';
 
 //Tab Sitemap
 $_lang['stercseo.sitemap'] = 'Google Sitemap';
 
-$_lang['stercseo.sitemap_include'] = 'Opnemen in Google Sitemap';
-$_lang['stercseo.sitemap_include_yes'] = 'Ja, neem deze pagina op in Google Sitemap';
-$_lang['stercseo.sitemap_include_no'] = 'Nee, verberg deze pagina voor Google Sitemap';
-$_lang['stercseo.sitemap_include_desc'] = 'Stel in of deze pagina wel of niet opgenomen mag worden in de sitemap voor Google';
+$_lang['stercseo.sitemap_include'] = 'Pagina opnemen in Google Sitemap?';
+$_lang['stercseo.sitemap_include_yes'] = 'Ja';
+$_lang['stercseo.sitemap_include_no'] = 'Nee';
+$_lang['stercseo.sitemap_include_desc'] = 'Stel in of deze pagina wel of niet mag worden opgenomen in de sitemap voor Google.';
 
 $_lang['stercseo.priority'] = 'Prioriteit';
 $_lang['stercseo.priority_important'] = '1.0 - Hoog';
 $_lang['stercseo.priority_normal'] = '0.5 - Normaal';
 $_lang['stercseo.priority_nopriority'] = '0.25 - Laag';
-$_lang['stercseo.priority_desc'] = 'Geef zoekmachines een indicatie van het belang van deze pagina door de prioriteit in te stellen. Let op: je prioriteit wordt niet blindelings overgenomen door zoekmachines.';
+$_lang['stercseo.priority_desc'] = 'Geef zoekmachines een indicatie van het belang van deze pagina door de prioriteit in te stellen. Let op: je prioriteit wordt niet blindelings overgenomen door zoekmachines!';
 
 $_lang['stercseo.changefreq'] = 'Update frequentie';
 $_lang['stercseo.changefreq_daily'] = 'Dagelijks';
 $_lang['stercseo.changefreq_weekly'] = 'Wekelijks';
 $_lang['stercseo.changefreq_monthly'] = 'Maandelijks';
-$_lang['stercseo.changefreq_desc'] = 'Geef aan hoe vaak je verwacht dat deze pagina verandert van inhoud.';
+$_lang['stercseo.changefreq_desc'] = 'Geef aan hoe vaak je verwacht dat deze pagina van inhoud verandert.';
 
 //Tab Redirects
 $_lang['stercseo.redirects'] = '301 Redirects';
-$_lang['stercseo.uri_add'] = '301 Redirect toevoegen';
-$_lang['stercseo.uri_header'] = 'Hieronder staat een overzicht van oude URL\'s voor deze pagina';
+$_lang['stercseo.uri_add'] = 'Een 301 redirect toevoegen';
+$_lang['stercseo.uri_header'] = 'Oude URL\'s van deze pagina:';
 $_lang['stercseo.grid_noresults'] = '<h4>Geen redirects</h4><p>Er zijn geen redirects voor deze pagina ingesteld.</p>';
-$_lang['stercseo.redirects_desc'] = 'Elke wijziging op je pagina heeft invloed op zoekmachines. Bij het wijzigen van de URL van een pagina betekent dit dat je álle opgebouwde zoekmachine-waarde (juice) kwijtraakt. Met 301 redirects raak je deze waarde niet kwijt. StercSEO voegt automatisch 301 redirects toe na het wijzigen van de URI van je pagina.';
-$_lang['stercseo.alreadyexists'] = '[[++site_URI]]<strong>[[+URI]]</strong> is al toegevoegd aan de pagina: <strong>[[+pagetitle]] ([[+id]])</strong>';
+$_lang['stercseo.redirects_desc'] = 'Wanneer je de URL van een pagina wijzigt, heeft dit ten gevolge dat je álle opgebouwde zoekmachine-waarde (juice) kwijtraakt. Met SEO Tab\'s 301 redirects houd je deze waarde vast! Om je te assisteren, voegt SEO Tab automatisch 301 redirects toe wanneer je de URL van een pagina wijzigt. Klik met je rechtermuisknop op een 301 redirect om deze te verwijderen.';
+$_lang['stercseo.alreadyexists'] = '<strong>[[+url]]</strong> is reeds toegevoegd aan de pagina: <strong>[[+pagetitle]] ([[+id]])</strong>.';
 $_lang['stercseo.uri_label'] = 'Oude URL';
-$_lang['stercseo.uri_label_desc'] = 'Voer de volledige url in. Bijvoorbeeld: "https://www.google.com/old-pages/about-us".';
-$_lang['stercseo.url_missing_protocol'] = 'Onjuiste url. Gelieve http:// of https:// toevoegen';
+$_lang['stercseo.uri_label_desc'] = 'Voer een volledige URL in. Bijvoorbeeld: "https://www.google.com/old-pages/about-us".';
+$_lang['stercseo.url_missing_protocol'] = 'Onjuiste URL. Voeg toe: http:// of https://';
 
 //Tab Freeze URL
 $_lang['stercseo.freeze_uri'] = 'Freeze URL';
-$_lang['stercseo.uri_override'] = 'Stel een Freeze URL in voor deze pagina';
+$_lang['stercseo.uri_override'] = 'Stel een Freeze URL in voor deze pagina.';
 $_lang['stercseo.uri_after'] = 'URL ná ';
-$_lang['stercseo.uri_after_desc'] = 'Freeze URLs kunnen worden gebruikt om korte urls te maken. 
-Om bijvoorbeeld de URL [[+site_url]]short-url" voor deze pagina te gebruiken, voer dan "short-url" in.';
+$_lang['stercseo.uri_after_desc'] = 'Freeze URL\'s kunnen worden gebruikt om URL\'s gebruiksvriendelijk te maken. 
+Om bijvoorbeeld de URL "[[+site_url]]gebruiksvriendelijke-url" voor deze pagina in te stellen, voer je "gebruiksvriendelijke-url" in.';
 
 //Settings
-$_lang['setting_stercseo.context-aware-alias'] = '301 Redirects are unique per context';
-$_lang['setting_stercseo.context-aware-alias_desc'] = 'Make old urls unique to context';
-$_lang['setting_stercseo.index'] = 'Default resource setting: Include in search engines';
-$_lang['setting_stercseo.index_desc'] = 'Include new pages in search engines per default';
-$_lang['setting_stercseo.follow'] = 'Default resource setting: Following links';
-$_lang['setting_stercseo.follow_desc'] = 'Follow links on new pages per default';
-$_lang['setting_stercseo.sitemap'] = 'Default resource setting: Include pages in the Google Sitemap';
-$_lang['setting_stercseo.sitemap_desc'] = 'Include new pages in sitemap.xml per default';
-$_lang['setting_stercseo.priority'] = 'Default resource setting: Priority';
-$_lang['setting_stercseo.priority_desc'] = 'Priority of page in sitemap.xml (0.25 or 0.5 or 1)';
-$_lang['setting_stercseo.changefreq'] = 'Default resource setting: Update frequency';
-$_lang['setting_stercseo.changefreq_desc'] = 'Default frequency (daily, weekly, monthly)';
-$_lang['setting_stercseo.hide_from_usergroups'] = 'Hide SEO Tab from these usergroups';
-$_lang['setting_stercseo.hide_from_usergroups_desc'] = 'Comma separated list of usergroups who are not allowed to access SEO Tab';
+$_lang['setting_stercseo.context-aware-alias'] = '301 redirects zijn uniek per context';
+$_lang['setting_stercseo.context-aware-alias_desc'] = 'Maak oude URL\'s uniek naar context';
+$_lang['setting_stercseo.index'] = 'Standaard bronnen setting: opnemen in zoekmachines';
+$_lang['setting_stercseo.index_desc'] = 'Neem nieuwe pagina\'s standaard op in zoekmachines';
+$_lang['setting_stercseo.follow'] = 'Standaard bronnen setting: links volgen';
+$_lang['setting_stercseo.follow_desc'] = 'Links op nieuwe pagina\'s standaard volgen';
+$_lang['setting_stercseo.sitemap'] = 'Standaard bronnen setting: pagina\'s opnemen in Google Sitemap';
+$_lang['setting_stercseo.sitemap_desc'] = 'Neem nieuwe pagina\'s standaard op in Google Sitemap';
+$_lang['setting_stercseo.priority'] = 'Standaard bronnen setting: prioriteit';
+$_lang['setting_stercseo.priority_desc'] = 'Prioriteit van de pagina in sitemap.xml (0.25 or 0.5 or 1)';
+$_lang['setting_stercseo.changefreq'] = 'Standaard bronnen setting: update frequentie';
+$_lang['setting_stercseo.changefreq_desc'] = 'Standaard frequentie (dagelijks, wekelijks, maandelijks)';
+$_lang['setting_stercseo.hide_from_usergroups'] = 'Verberg SEO Tab voor deze gebruikersgroepen';
+$_lang['setting_stercseo.hide_from_usergroups_desc'] = 'Komma gescheiden lijst met gebruikersgroepen die geen toegang hebben tot SEO Tab';
 
 // CMP
-$_lang['stercseo.redirects.description'] = 'Here you can view and manage your 301 redirects. 
-Redirects can also be added from the resource create and update pages.';
-$_lang['stercseo.redirects.window_title'] = 'Add redirect url';
-$_lang['stercseo.uri'] = 'Old url (url to redirect)';
-$_lang['stercseo.target'] = 'Target resource / url';
+$_lang['stercseo.redirects.description'] = 'Hier kun je je 301 redirects bekijken en beheren. Redirects kunnen ook worden toegevoegd vanuit de bron maak- en updatepagina\'s.';
+$_lang['stercseo.redirects.window_title'] = 'Redirect URL toevoegen';
+$_lang['stercseo.uri'] = 'Oude URL (URL naar redirect)';
+$_lang['stercseo.target'] = 'Nieuwe URL / Bron';
 $_lang['stercseo.uri_update'] = 'Update redirect';
-$_lang['stercseo.uri_remove'] = 'Remove redirect';
-$_lang['stercseo.uri_remove_confirm'] = 'Are you sure you want to remove this redirect?';
-$_lang['stercseo.migrate'] = 'Migrate redirects';
-$_lang['stercseo.migrate_desc'] = 'Here you can migrate your redirects (SEO Tab version 1.2.2 and below) from resource properties to seoUrl objects. This page will automatically migrate all the redirects for you, so no action is required, but please keep this page open for SEO Tab to correctly handle the migration process.';
-$_lang['stercseo.migrate_alert'] = 'Your SEO Tab redirects need to be migrated. Click here to visit the migration page.';
+$_lang['stercseo.uri_remove'] = 'Verwijder redirect';
+$_lang['stercseo.uri_remove_confirm'] = 'Weet je zeker dat je deze redirect wil verwijderen?';
+$_lang['stercseo.migrate'] = 'Migreer redirects';
+$_lang['stercseo.migrate_desc'] = 'Hier kun je je redirects (SEO Tab versie 1.2.2 en ouder) migreren van resource properties naar seoUrl objects. Deze pagina migreert al je redirects automatisch, een handeling is dus niet vereist. Houd a.u.b. wel deze pagina open zodat SEO Tab het migratieproces correct kan afhandelen.';
+$_lang['stercseo.migrate_alert'] = 'Je SEO Tab redirects moeten worden gemigreerd. Klik hier om de migratiepagina te bezoeken.';
 $_lang['stercseo.migrate_status'] = 'Status';
-$_lang['stercseo.migrate_running'] = 'Currently running migration process. Please keep this page open in your browser.';
-$_lang['stercseo.migrate_success'] = 'Migration completed';
-$_lang['stercseo.migrate_success_msg'] = 'All your redirects have been successfully migrated.';
+$_lang['stercseo.migrate_running'] = 'Het migratieproces loopt momenteel. Houd deze pagina a.u.b. open in je browser.';
+$_lang['stercseo.migrate_success'] = 'Migratieproces voltooid.';
+$_lang['stercseo.migrate_success_msg'] = 'Al je redirects zijn succesvol gemigreerd.';
